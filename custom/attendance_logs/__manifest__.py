1c2ab004a985d693009a757aa6fe5095916f7f0a
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2023-08-01 11:58:47 +0300

    Attendance

---
+++ custom/attendance_logs/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employee's Attendance Tracking ",
+    'summary': """
+       It’s used to track Employee’s Daily Attendance   """,
+    'description': """
+    Employee Attendance Check-in and Check-out Activities will be tracked by each day.
+    """,
+    'author': "AppsComp Widgets Pvt Ltd",
+    'website': "http://www.appscomp.com",
+    'license': 'LGPL-3',
+    "live_test_url": "https://www.youtube.com/watch?v=Fub-7Rq_F9U&feature=youtu.be",
+    'images': ['static/description/Employee_Attendance.gif'],
+    'category': 'Hr',
+    'version': '16.0',
+    'price': 10,
+    'depends': ['base', 'hr', 'hr_attendance'],
+    'data': ['views/attendance_view.xml'],
+    'demo': [],
+
+}
