9626a00513bc1fe2ea11bc313f9d5d576382c1dd
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2023-10-24 11:21:29 +0300

    custom_pos

--- custom/limit_cash_control/__manifest__.py
+++ custom/limit_cash_control/__manifest__.py
@@ -9,7 +9,7 @@
     "depends": ["point_of_sale"],
     'installable': True,
     'application': False,
-    'data': ['pos_config.xml','custom_pos.xml'],
+    'data': ['pos_config.xml'],
     "price": 150,
     "currency": 'USD',
     'assets': {
