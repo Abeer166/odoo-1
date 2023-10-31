a0c23d8965abf4fb099ebd8d39ece6b774f05878
najwa-56 <najwa-56@hotmail.com>
2023-10-31 13:38:50 +0300

    multi uom

--- addons/sales_multi_uom/models/sale.py
+++ addons/sales_multi_uom/models/sale.py
@@ -36,12 +36,36 @@ class product_product(models.Model):
             else:
                 record.selected_uom_ids = []
 
+#testttttttt
 
+class product_template(models.Model):
+    _inherit = 'sale.order.template.line'
+
+
+    selected_uom_ids = fields.Many2many(string="Uom Ids", related='product_id.selected_uom_ids')
+    sales_multi_uom_id = fields.Many2one("wv.sales.multi.uom", string="Cust UOM", domain="[('id', '=', selected_uom_ids)]")
+
+    def _prepare_order_line_values(self):
+        """ Give the values to create the corresponding order line.
+
+        :return: `sale.order.line` create values
+        :rtype: dict
+        """
+        self.ensure_one()
+        return {
+            'display_type': self.display_type,
+            'name': self.name,
+            'product_id': self.product_id.id,
+            'sales_multi_uom_id': self.sales_multi_uom_id,
+            'product_uom_qty': self.product_uom_qty,
+            'product_uom': self.product_uom_id.id,
+        }
+#testttttttt
 class sale_order_line(models.Model):
     _inherit = "sale.order.line"
 
     selected_uom_ids = fields.Many2many(string="Uom Ids", related='product_id.selected_uom_ids')
-    sales_multi_uom_id = fields.Many2one("wv.sales.multi.uom", string="Cust UOM", domain="[('id', 'in', selected_uom_ids)]")
+    sales_multi_uom_id = fields.Many2one("wv.sales.multi.uom", string="Cust UOM" , domain="[('id', '=', selected_uom_ids)]")
     
 
     @api.onchange('sales_multi_uom_id')
