27b019ac10b05eb459e2bf714e9b86482f683eeb
AbeerAlmushigh <56722657+Abeer166@users.noreply.github.com>
2024-02-07 14:39:10 +0300

    edit/editing

--- custom/customer_route_management/models/inherit_partner.py
+++ custom/customer_route_management/models/inherit_partner.py
@@ -12,12 +12,11 @@ class ResPartner(models.Model):
     locations = fields.Many2one('route.lines', string='Location')
     sequence = fields.Integer(default=10)
     name=fields.Char("أسم العميل")
-
     statuss = fields.Boolean(string="الحالة")
 
     def get_all_dues(self):
         query = """select name,invoice_date_due,amount_residual_signed from account_move where partner_id in
-                (select id from res_partner where id =%s or parent_id=%s) and state != 'draft' and 
+                (select id from res_partner where id =%s or parent_id=%s) and state != 'draft' and
                 amount_residual_signed != 0
                 order by create_date"""
         self.env.cr.execute(query, [self.id, self.id])
